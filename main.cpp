755bd36060c1c2ac5160b990fd88fb7eb5786c22
hikari <hikari39764@gmail.com>
2026-04-19 23:35:42 +0900

    00_01Fin

--- main.cpp
+++ main.cpp
@@ -1,4 +1,56 @@
 #include <Novice.h>
+#include <cmath> 
+
+// 3次元ベクトルの構造体
+struct Vector3 {
+	float x, y, z;
+};
+
+// --- 加算 ---
+Vector3 Add(const Vector3& v1, const Vector3& v2) {
+	return { v1.x + v2.x, v1.y + v2.y, v1.z + v2.z };
+}
+
+// --- 減算 ---
+Vector3 Subtract(const Vector3& v1, const Vector3& v2) {
+	return { v1.x - v2.x, v1.y - v2.y, v1.z - v2.z };
+}
+
+// --- スカラー倍 ---
+Vector3 Multiply(float scalar, const Vector3& v) {
+	return { scalar * v.x, scalar * v.y, scalar * v.z };
+}
+
+// --- 内積 ---
+float Dot(const Vector3& v1, const Vector3& v2) {
+	return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+}
+
+// --- 長さ（ノルム） ---
+float Length(const Vector3& v) {
+	return sqrtf(v.x * v.x + v.y * v.y + v.z * v.z);
+}
+
+// --- 正規化 ---
+Vector3 Normalize(const Vector3& v) {
+	float len = Length(v);
+	if (len != 0) {
+		return { v.x / len, v.y / len, v.z / len };
+	}
+	return { 0, 0, 0 };
+}
+
+// 数値表示用の設定
+static const int kColumnWidth = 60;
+static const int kRowHeight = 20;
+
+// 3次元ベクトルの数値表示関数
+void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label) {
+	Novice::ScreenPrintf(x, y, "%.02f", vector.x);
+	Novice::ScreenPrintf(x + kColumnWidth, y, "%.02f", vector.y);
+	Novice::ScreenPrintf(x + kColumnWidth * 2, y, "%.02f", vector.z);
+	Novice::ScreenPrintf(x + kColumnWidth * 3, y, "%s", label);
+}
 
 const char kWindowTitle[] = "LC1C_14_コウケンリュウ";
 
@@ -8,9 +60,14 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
 
+	// --- データ ---
+	Vector3 v1{ 1.0f, 3.0f, -5.0f };
+	Vector3 v2{ 4.0f, -1.0f, 2.0f };
+	float k = 4.0f;
+
 	// キー入力結果を受け取る箱
-	char keys[256] = {0};
-	char preKeys[256] = {0};
+	char keys[256] = { 0 };
+	char preKeys[256] = { 0 };
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -25,6 +82,14 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
+		// 各種計算の実行
+		Vector3 resultAdd = Add(v1, v2);
+		Vector3 resultSubtract = Subtract(v1, v2);
+		Vector3 resultMultiply = Multiply(k, v1);
+		float resultDot = Dot(v1, v2);
+		float resultLength = Length(v1);
+		Vector3 resultNormalize = Normalize(v2);
+
 		///
 		/// ↑更新処理ここまで
 		///
@@ -32,6 +97,14 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+		// 結果の描画
+		VectorScreenPrintf(0, 0, resultAdd, "  : Add");
+		VectorScreenPrintf(0, kRowHeight, resultSubtract, "  : Subtract");
+		VectorScreenPrintf(0, kRowHeight * 2, resultMultiply, "  : Multiply");
+		Novice::ScreenPrintf(0, kRowHeight * 3, "%.02f  : Dot", resultDot);
+		Novice::ScreenPrintf(0, kRowHeight * 4, "%.02f  : Length", resultLength);
+		VectorScreenPrintf(0, kRowHeight * 5, resultNormalize, "  : Normalize");
 
 		///
 		/// ↑描画処理ここまで
